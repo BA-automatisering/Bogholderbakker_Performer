3a795b485ffd11190840ab676a119e3146c36f5f
Leif Junge Pedersen <lejp@aarhus.dk>
2026-06-22 14:37:07 +0200

    Tilretning af body

--- robot_framework/lists.py
+++ robot_framework/lists.py
@@ -63,7 +63,7 @@ def send_driftliste(orchestrator_connection: OrchestratorConnection, process_nam
     header = process_name+"  "+x.strftime("%d-%b-%Y")
     body = ""
     while n < len(globals.driftliste):
-        body = body + globals.driftliste[n]["status"]+"\t"+globals.driftliste[n]["message"]+"\t"+str(globals.driftliste[n]["start_date"])+"\t"+globals.driftliste[n]["created_by"]+"\t"+globals.driftliste[n]["data"]+"<br>"
+        body = body + "Status: "+globals.driftliste[n]["status"]+" - Message: "+globals.driftliste[n]["message"]+" - Starttid: "+str(globals.driftliste[n]["start_date"])+" - Dannet af: "+globals.driftliste[n]["created_by"]+" - Data: "+globals.driftliste[n]["data"]+"<br>"
         n += 1
     
     html_message = f"""
